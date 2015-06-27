6b5c0c2369b84128b77e5af1f7b57a606fb4dc5c
Karl Fathi <karl.fathi@gmail.com>
2015-06-27 14:23:34 -0300

    Wordpress Stuff - Add: Function to disable WP Emojis - Add: Constants to prevent WPML from loading useless assets.

--- htdocs/content/themes/jacobsen-arquitetura/app/config/constants.config.php
+++ htdocs/content/themes/jacobsen-arquitetura/app/config/constants.config.php
@@ -4,11 +4,16 @@ return array(
 
 	/*
 	* Edit this file in order to configure your application's
-	* constants. 
+	* constants.
 	*
 	* The key is the constant NAME (be sure to write it capitals)
 	* and key's value is the constant VALUE.
 	*/
-	'APPVERSION'	=> '1.0'
-
+	'APPVERSION'                          => '1.0',
+    /*
+    * Prevent WPML/Sitepress from loading their useless CSS/JS
+    */
+    'ICL_DONT_LOAD_NAVIGATION_CSS'        => true,
+    'ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS' => true,
+    'ICL_DONT_LOAD_LANGUAGES_JS'          => true
 );
