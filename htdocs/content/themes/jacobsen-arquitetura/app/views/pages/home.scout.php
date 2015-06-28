26c710274ec0f1320a5944ca56f274a92a554cb0
Karl Fathi <karl.fathi@gmail.com>
2015-06-28 18:22:40 -0300

    Fix: Double quotes => Single quotes Add: More text to home temporary template to test wrapping Add: Repeated background line to test out sassline settings

--- htdocs/content/themes/jacobsen-arquitetura/app/views/pages/home.scout.php
+++ htdocs/content/themes/jacobsen-arquitetura/app/views/pages/home.scout.php
@@ -2,5 +2,6 @@
 
 @section('main')
     <h1>Home template</h1>
-    <p>Before they sold out ennui crucifix meditation PBR&amp;B. Scenester letterpress asymmetrical Marfa leggings, actually keytar McSweeney's heirloom Brooklyn chambray kitsch street art squid.</p>
+    <p>Before they sold out ennui crucifix meditation PBR&amp;B. Scenester letterpress asymmetrical Marfa leggings, actually keytar McSweeney's heirloom Brooklyn chambray kitsch street art squid. Before they sold out ennui crucifix meditation PBR&amp;B. Scenester letterpress asymmetrical Marfa leggings, actually keytar McSweeney's heirloom Brooklyn chambray kitsch street art squid.</p>
+    <p>Before they sold out ennui crucifix meditation PBR&amp;B. Scenester letterpress asymmetrical Marfa leggings, actually keytar McSweeney's heirloom Brooklyn chambray kitsch street art squid. Before they sold out ennui crucifix meditation PBR&amp;B. Scenester letterpress asymmetrical Marfa leggings, actually keytar McSweeney's heirloom Brooklyn chambray kitsch street art squid.</p>
 @overwrite
