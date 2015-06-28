d6ce124b1d8c2fe7f0732732bc48658540f8acb0
Karl Fathi <karl.fathi@gmail.com>
2015-06-27 21:53:45 -0300

    - Refactor: CSS classes for font loading - Refactor: JS font-loading now uses FontObserver and WebFontLoader - Add: Webfontloader dependency

--- htdocs/content/themes/jacobsen-arquitetura/app/assets/src/scripts/js/modules/fonts/loader.js
+++ htdocs/content/themes/jacobsen-arquitetura/app/assets/src/scripts/js/modules/fonts/loader.js
@@ -1,20 +1,41 @@
+/* global FontFaceObserver */
 'use strict';
 
-var fontfaceobserver = require('fontfaceobserver');
+require('fontfaceobserver');
+var webfontloader = require('webfontloader');
 
 module.exports = function() {
-  var geogrotesque300 = new fontfaceobserver('geogrotesque', {weight:300});
-  var geogrotesque400 = new fontfaceobserver('geogrotesque', {weight:400});
-  var geogrotesque600 = new fontfaceobserver('geogrotesque', {weight:600});
-  var exposerifpro400 = new fontfaceobserver('expo-serif-pro', {weight:400});
+  // Font Loader
 
-  Promise.all(
+  webfontloader.load({
+    typekit: {id:'idx2gzm'}
+  });
+
+  // Font Observer
+  var geogrotesque300 = new FontFaceObserver('geogrotesque', {weight:300});
+  var geogrotesque400 = new FontFaceObserver('geogrotesque', {weight:400});
+  var geogrotesque600 = new FontFaceObserver('geogrotesque', {weight:600});
+  var exposerifpro400 = new FontFaceObserver('expo-serif-pro', {weight:400});
+
+  Promise.all([
     geogrotesque300.check(),
     geogrotesque400.check(),
-    geogrotesque600.check(),
+    geogrotesque600.check()
+  ]).then(function() {
+    window.document.documentElement.className += ' type__heading--available';
+    console.log('Heading fonts loaded');
+  }, function() {
+    window.document.documentElement.className += ' type__heading--unavailable';
+    console.log('Heading fonts unavailable');
+  });
+
+  Promise.all([
     exposerifpro400.check()
-  ).then(function() {
-    window.document.documentElement.className += ' fonts-loaded';
-    console.log('Fonts loaded');
+  ]).then(function() {
+    window.document.documentElement.className += ' type__body--available';
+    console.log('Body fonts loaded');
+  }, function() {
+    window.document.documentElement.className += ' type__body--unavailable';
+    console.log('Body fonts unavailable');
   });
 };
