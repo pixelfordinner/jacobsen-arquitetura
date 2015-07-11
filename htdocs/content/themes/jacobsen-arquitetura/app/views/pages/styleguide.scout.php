bd4c9e6330f46d4af3231137e8159a227a6581c8
Karl Fathi <karl.fathi@gmail.com>
2015-07-10 21:28:57 -0300

    Styleguide - Add: Styleguide scout view - Add: Styleguide styles as a separate css file

---
+++ htdocs/content/themes/jacobsen-arquitetura/app/views/pages/styleguide.scout.php
@@ -0,0 +1,37 @@
+@extends('layouts.master')
+
+@section('main')
+    <h1 class="heading--alpha">Styleguide</h1>
+
+    <hr>
+
+    <h2 class="heading--beta">Typography</h2>
+    <p>Jacobsen Arquitetura uses two main fonts: <a href="https://www.myfonts.com/fonts/emtype/geogrotesque/" target="_blank">Geogrotesque</a> and <a href="https://typekit.com/fonts/expo-serif-pro" target="_blank">Expo Serif Sans</a>.<br> Modular scales are used to setup heading and body text base sizes, along with being responsive to every screen size<br> Vertical Rhythm along with baseline set on x-height are used to achieve maximum readability and clarity.<br> Lastly, font-kerning, ligatures are activated once the fonts are loaded.</p>
+
+    <hr>
+
+    <h2 class="heading--beta">Headings</h2>
+
+    <h1 class="heading--alpha">Header Level 1 (alpha)</h1>
+    <h2 class="heading--beta">Header Level 2 (beta)</h2>
+    <h3 class="heading--gamma">Header Level 3 (gamma)</h3>
+    <h4 class="heading--delta">Header Level 4 (delta)</h4>
+    <h5 class="heading--epsilon">Header Level 5 (epsilon)</h5>
+    <h6 class="heading--zeta">Header Level 6 (zeta)</h6>
+
+    <hr>
+
+    <h2 class="heading--beta">Body Text</h2>
+
+    <p>Before they sold out ennui crucifix meditation PBR&amp;B. Scenester letterpress asymmetrical Marfa leggings, actually keytar McSweeney's heirloom Brooklyn chambray kitsch street art squid. Before they sold out ennui crucifix meditation PBR&amp;B. Scenester letterpress asymmetrical Marfa leggings, actually keytar McSweeney's heirloom Brooklyn chambray kitsch street art squid.</p>
+    <p>Before they sold out ennui crucifix meditation PBR&amp;B. Scenester letterpress asymmetrical Marfa leggings, actually keytar McSweeney's heirloom Brooklyn chambray kitsch street art squid. Before they sold out ennui crucifix meditation PBR&amp;B. Scenester letterpress asymmetrical Marfa leggings, actually keytar McSweeney's heirloom Brooklyn chambray kitsch street art squid.</p>
+
+    <hr>
+
+    <h2 class="heading--beta">Buttons</h2>
+
+    <a href="#" class="button heading--zeta">Presskit</a>
+    <a href="#" class="button heading--zeta uppercase">Presskit</a>
+
+    <hr>
+@overwrite
