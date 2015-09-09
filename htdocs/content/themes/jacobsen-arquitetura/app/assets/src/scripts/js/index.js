cc68be81811f1da5c73118ac92430dce6c2e6b53
Karl Fathi <karl.fathi@gmail.com>
2015-09-09 18:40:12 -0300

    Add: New script to async load google maps when needed

--- htdocs/content/themes/jacobsen-arquitetura/app/assets/src/scripts/js/index.js
+++ htdocs/content/themes/jacobsen-arquitetura/app/assets/src/scripts/js/index.js
@@ -3,7 +3,9 @@
 var browserFeatures = require('helpers/browser-features');
 var fontloader = require('fonts/loader');
 var keyEvents = require('helpers/key-events');
+var gmaps = require('maps/gmaps');
 
 browserFeatures();
 fontloader();
 keyEvents();
+gmaps();
---
+++ htdocs/content/themes/jacobsen-arquitetura/app/assets/src/scripts/js/modules/maps/gmaps.js
@@ -0,0 +1,105 @@
+'use strict';
+
+var $ = require('jquery');
+var googleMapsLoader = null;
+
+var settings = {
+  zoom: 12,
+  scrollwheel: false,
+  draggable: false,
+  disableDefaultUI: true,
+  styles: [{
+    'featureType': 'landscape',
+    'elementType': 'labels',
+    'stylers': [{
+      'visibility': 'off'
+    }]
+  }, {
+    'featureType': 'transit',
+    'elementType': 'labels',
+    'stylers': [{
+      'visibility': 'off'
+    }]
+  }, {
+    'featureType': 'poi',
+    'elementType': 'labels',
+    'stylers': [{
+      'visibility': 'off'
+    }]
+  }, {
+    'featureType': 'water',
+    'elementType': 'labels',
+    'stylers': [{
+      'visibility': 'off'
+    }]
+  }, {
+    'featureType': 'road',
+    'elementType': 'labels.icon',
+    'stylers': [{
+      'visibility': 'off'
+    }]
+  }, {
+    'stylers': [{
+      'hue': '#00aaff'
+    }, {
+      'saturation': -100
+    }, {
+      'gamma': 2.15
+    }, {
+      'lightness': 12
+    }]
+  }, {
+    'featureType': 'road',
+    'elementType': 'labels.text.fill',
+    'stylers': [{
+      'visibility': 'on'
+    }, {
+      'lightness': 24
+    }]
+  }, {
+    'featureType': 'road',
+    'elementType': 'geometry',
+    'stylers': [{
+      'lightness': 57
+    }]
+  }]
+};
+
+var marker = {
+  draggable: false,
+  raiseOnDrag: false
+};
+
+var gmaps = function() {
+  $('[data-gmap-lat][data-gmap-lng]').each(gmaps.iterateMaps);
+};
+
+gmaps.iterateMaps = function(i, element) {
+  var $element = $(element);
+  googleMapsLoader = googleMapsLoader || require('google-maps');
+
+  googleMapsLoader.load(function(google) {
+    var coordinates = new google.maps.LatLng(
+      $element.data('gmap-lat'),
+      $element.data('gmap-lng')
+    );
+
+    settings.center = coordinates;
+
+    if ($element.data('gmap-icon')) {
+      marker.icon = $element.data('gmap-icon');
+      marker.title = $element.data('gmap-icon-title');
+    }
+
+    var map = new google.maps.Map($element.get(0), settings);
+
+    marker.position = coordinates;
+    marker.map = map;
+
+    new google.maps.Marker(marker);
+
+    console.log(marker);
+  });
+};
+
+module.exports = gmaps;
