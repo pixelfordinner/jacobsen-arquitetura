154c774e7a775614a13a9d0e66c034765f2738f0
Karl Fathi <karl.fathi@gmail.com>
2015-09-16 18:51:12 -0300

    - Tweak: Wait for document to be ready before js - Add: Picturefill dependency

--- htdocs/content/themes/jacobsen-arquitetura/app/assets/src/scripts/js/index.js
+++ htdocs/content/themes/jacobsen-arquitetura/app/assets/src/scripts/js/index.js
@@ -1,11 +1,19 @@
 'use strict';
 
+// User modules
 var browserFeatures = require('helpers/browser-features');
 var fontloader = require('fonts/loader');
 var keyEvents = require('helpers/key-events');
 var gmaps = require('maps/gmaps');
 
-browserFeatures();
-fontloader();
-keyEvents();
-gmaps();
+// Vendor modules
+var $ = require('jquery');
+var picturefill = require('picturefill');
+
+$(document).ready(function() {
+  fontloader();
+  browserFeatures();
+  picturefill();
+  keyEvents();
+  gmaps();
+});
