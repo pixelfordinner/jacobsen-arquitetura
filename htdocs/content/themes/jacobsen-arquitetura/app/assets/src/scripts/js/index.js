542da697163f87afcb4adc26211f20e0287cd182
Karl Fathi <karl.fathi@gmail.com>
2015-09-27 15:11:34 -0300

    Page transitions
    - Add: Smoothstate dependency
    - Add: Fadein transition on page change

--- htdocs/content/themes/jacobsen-arquitetura/app/assets/src/scripts/js/index.js
+++ htdocs/content/themes/jacobsen-arquitetura/app/assets/src/scripts/js/index.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// Vendor modules
+var $ = require('jquery');
+window.jQuery = $;
+var picturefill = require('picturefill');
+
 // User modules
 var browserFeatures = require('helpers/browser-features');
 var fontloader = require('fonts/loader');
@@ -7,17 +12,26 @@ var imagesLoading = require('helpers/images-loading');
 var keyEvents = require('helpers/key-events');
 var gmaps = require('maps/gmaps');
 var slickCarousel = require('sliders/slick-carousel');
-
-// Vendor modules
-var $ = require('jquery');
-var picturefill = require('picturefill');
+var pageTransitions = require('helpers/page-transitions');
 
 $(document).ready(function() {
-  fontloader();
-  browserFeatures();
-  picturefill();
-  imagesLoading();
-  keyEvents();
-  gmaps();
-  slickCarousel();
+  var baseModules = function() {
+    fontloader();
+    browserFeatures();
+    keyEvents();
+  };
+
+  var contentModules = function() {
+    gmaps();
+    picturefill();
+    imagesLoading();
+    slickCarousel();
+    pageTransitions();
+  };
+
+  baseModules();
+  contentModules();
+
+  $(window).on('base-modules', contentModules);
+  $(window).on('content-modules', contentModules);
 });
