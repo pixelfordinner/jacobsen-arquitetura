116ccf5fb53c8386b8661821ea905138fb17b666
Karl Fathi <karl.fathi@gmail.com>
2015-09-28 13:27:30 -0300

    Projects archive pagination
    - Add: Pagination Support

--- htdocs/content/themes/jacobsen-arquitetura/app/views/cpt/projects/archive.scout.php
+++ htdocs/content/themes/jacobsen-arquitetura/app/views/cpt/projects/archive.scout.php
@@ -6,7 +6,7 @@
         <a href="{{ get_term_link($category) }}" class="button button--small button--filter uppercase @if($category->slug == $current_category)button--active @endif" data-category-id="{{ $category->term_id }}">{{ $category->name }}</a>
 @endforeach
     </div>
-    <section class="row--vpadded row--flex projects-grid">
+    <section class="row--vpadded row--flex projects-grid" data-projects-grid>
 @loop
 <?php $post_fields = get_fields(); ?>
         <div class="projects-grid__item">
@@ -14,13 +14,19 @@
                 <h2 class="heading--delta heading--light uppercase projects-grid__item--title">{{ Loop::title() }}</h2>
                 <ul class="projects-grid__item__extras">
                     <li class="projects-grid__item__extras__item heading--iota heading--light uppercase">{{ $post_fields['date_end'] ? $post_fields['date_end'] : __('In progress') }}</li>
-                    <li class="projects-grid__item__extras__item heading--iota heading--light uppercase">{{ $post_fields['category_featured']->name }}</li>
+                    <li class="projects-grid__item__extras__item heading--iota heading--light uppercase">{{ is_object($post_fields['category_featured']) ? $post_fields['category_featured']->name : __('None') }}</li>
                 </ul>
             </div>
             <a class="projects-grid__item--link" href="{{ Loop::link() }}">
-                {{ Macros::responsive_image(get_post_thumbnail_id(), 'project_thumbnails', array('project-grid__item__thumbnail', 'image--loading')) }}
+                {{ Macros::responsive_image(get_post_thumbnail_id(), 'project_thumbnails', array('project-grid__item__thumbnail')) }}
             </a>
         </div>
 @endloop
     </section>
+<?php $matches = array(); ?>
+@if(preg_match("/<a\s+(?:[^>]*?\s+)?href=\"([^\"]*)\"/", get_next_posts_link(), $matches) == 1)
+    <div class="row--vpadded project-grid__pagination">
+        <a href="{{ $matches[1] }}" class="button uppercase page-transition--none" data-next-projects>{{ Macros::symbol('symbols-download', 'Download', array('button__symbol')) }}{{ __('See more') }}</a>
+    </div>
+@endif
 @overwrite
