a0fc53d44a922f2ccb7ce3864a20e5ab774ee4ae
Karl Fathi <karl.fathi@gmail.com>
2019-05-30 16:48:06 -0300

    Fix: Method for injecting Gmaps api key in ACF

--- htdocs/content/themes/jacobsen-arquitetura/resources/admin/actions.php
+++ htdocs/content/themes/jacobsen-arquitetura/resources/admin/actions.php
@@ -1,7 +1,9 @@
 <?php
 
- Action::add('pre_get_posts', 'ProjectController');
+Action::add('pre_get_posts', 'ProjectController');
 
- add_action('acf/init', function () {
-     acf_update_setting('google_api_key', 'AIzaSyDn2-8djSEe3QYLURqcIFY27rqTD3AWMpI');
- });
+add_filter('acf/fields/google_map/api', function ($api) {
+    $api['key'] = 'AIzaSyDn2-8djSEe3QYLURqcIFY27rqTD3AWMpI';
+
+    return $api;
+});
